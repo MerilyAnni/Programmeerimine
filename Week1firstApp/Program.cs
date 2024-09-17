7f7e0fb7ad3c67b4712038f17b33c4b6bea790c4
MerilyAnni <annimerily@gmail.com>
2024-09-17 17:33:06 +0300

    My first app

---
+++ Week1firstApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string myName = "merily";
+string myLastName = "An";
+//Console.WriteLine("hello" + " " + myName);
+Console.WriteLine($"Hello, {myName} {myLastName}!");
